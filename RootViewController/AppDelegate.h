7f22482d3f97e1be0edf531c736164333a24cac2
XmL <mac@bogon.(none)>
2013-03-19 10:18:32 +0800

    Initial Commit

---
+++ RootViewController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RootViewController
+//
+//  Created by XmL on 13-3-19.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RootViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RootViewController
+//
+//  Created by XmL on 13-3-19.
+//  Copyright (c) 2013年 XmL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
